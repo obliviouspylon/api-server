41e4bfcbd80f78da5646ab5150044dd54d74c559
James Luo <contact@jamesckluo.ca>
2022-12-08 03:31:46 -0500

    Add TextNow SMS Messaging

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ import argparse
 import importlib
 import datetime
 from pytz import timezone
+import time
+import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', default=9000, dest='port',
@@ -24,7 +26,7 @@ listenOn = "0.0.0.0"
 gas_gasWizard = importlib.import_module("gas-price-notification.gasWizard")
 gas_jsonController = importlib.import_module("gas-price-notification.jsonController")
 gas_Enpro680 = importlib.import_module("gas-price-notification.Enpro680")
-
+gas_TextNow = gas_Enpro680 = importlib.import_module("gas-price-notification.TextNow")
 
 def hourisbetween(start, end):
     now = datetime.datetime.now(timezone("EST")).hour
@@ -71,7 +73,7 @@ def sendPrediction():
             message = message + site + ": " + direction + " by " + amount + " to " + price + "¢/L\n"
 
     if message == "":
-        return Response("No prediction found for " + tomorrowDate.strftime("%b %d"), status=201)
+        return ("No prediction found for " + tomorrowDate.strftime("%b %d"))
     else:
         message = "Gas Prediction - " + tomorrowDate.strftime("%b %d") + "\n" + message
         return(message[:-1])
@@ -80,6 +82,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 userKey = os.getenv('USER_KEY')
+tn_username = os.getenv('TEXTNOW_USERNAME')
+tn_sid = os.getenv('TEXTNOW_SID')
+tn_csrf = os.getenv('TEXTNOW_CSRF')
 
 @app.route('/gas/user',methods = ['POST', 'GET', 'PUT', "DELETE"])
 def manageUsers():
@@ -107,6 +112,7 @@ def manageUsers():
         except:
             return ("")
         if gas_jsonController.addUser(number):
+            gas_TextNow.send_sms(number,"Gas Price Notification. Number Added. To remove, please text STOP.", tn_username, tn_sid, tn_csrf)
             return ("Successful")
         else:
             return ("Failed")
@@ -118,6 +124,7 @@ def manageUsers():
         except:
             return ("")
         if gas_jsonController.deleteUser(number):
+            gas_TextNow.send_sms(number,"Gas Price Notification. Number Flagged for removal. Please give a few days for processing.", tn_username, tn_sid, tn_csrf)
             return ("Successful")
         else:
             return ("Failed")
@@ -125,6 +132,50 @@ def manageUsers():
         print("Ignore")
         return("")
 
+@app.route('/gas/textnow',methods = ['POST'])
+def textnow_update():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        reqJson = request.json
+    else:
+        print('Content-Type not supported!')
+        return ("")
+
+    try:
+        if reqJson["Key"] != userKey:
+            return("")
+    except:
+        return ("")
+    
+    if "TEXTNOW_USERNAME" in reqJson:
+        tn_username = reqJson["TEXTNOW_USERNAME"]
+    if "TEXTNOW_SID" in reqJson:
+        tn_sid = reqJson["TEXTNOW_SID"]
+    if "TEXTNOW_CSRF" in reqJson:
+        tn_csrf = reqJson["TEXTNOW_CSRF"]
+    return("Done")
+
+
+@scheduler.task('interval', id='send_gas_SMS', minutes=130, misfire_grace_time=900)
+# @app.route('/textnow')
+def send_gas_prediction():
+    if hourisbetween(12, 21):
+        message = sendPrediction().replace("\n", ". ")
+
+        try:
+            with open("gasPredictionMessage.txt","r") as f:
+                contents = f.read()
+        except:
+            contents = ""
+        
+        if message != contents:
+            with open("gasPredictionMessage.txt","w") as f:
+                f.write(message)
+            numbers = gas_jsonController.readUsers()
+            for number in numbers:
+                time.sleep(random.randint(5,20))
+                gas_TextNow.send_sms(number,message, tn_username, tn_sid, tn_csrf)
+    return("")
 
 #---Flights in Radius---------------------------
 flightsRadius = importlib.import_module("flights-in-radius.flights_in_radius")
